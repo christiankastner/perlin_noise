491a8e3888d3a6b4facfeac3371c5e1bf3078193
Christian Kastner <christianmkastner@gmail.com>
2020-03-11 11:09:36 -0700

    saving merge conflict

---
+++ particle.js
@@ -0,0 +1,44 @@
+function Particle() {
+    this.pos = createVector(random(width/4, 3 * width/4) ,random(height/4, 3 * height/4));
+    this.acc = createVector(0,0);
+    this.maxspeed = 1;
+    this.vel = createVector(0,0);
+    this.prevPos = this.pos.copy();
+    this.h = 
+
+
+    this.update = function() {
+        this.prevPos = this.pos.copy()
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.vel.limit(this.maxspeed);
+        this.acc.mult(0);
+    }
+
+    this.follow = function(vectors) {
+        let x = floor(this.pos.x / scl);
+        let y = floor(this.pos.y / scl);
+        let index = x + y + cols;
+        let force = vectors[index];
+        this.applyForce(force);
+    }
+
+    this.applyForce = function (force) {
+        this.acc.add(force)
+
+    }
+
+    this.show = function() {
+        stroke(0, noise(zoff)*20);
+        strokeWeight(2)
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y)
+    }
+
+    this.edges = function () {
+        if (this.pos.x > width) this.pos.x = random(width/4, 3 * width/4);
+        if (this.pos.x < 0) this.pos.x = random(width/4, 3 * width/4);
+        if (this.pos.y > height) this.pos.y = 0;
+        if (this.pos.y < 0) this.pos.y = height;
+        
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,54 @@
-let img;
+let inc = .01;
+var scl = 10;
+let cols, rows;
+let zoff = 0;
+let particles = [];
+let flowField
+
+
 function setup() {
-  createCanvas(600, 600);
-  img = loadImage('background.png')
+  createCanvas(500, 500);
+  colorMode(HSB, 255)
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+
+  flowField = new Array (cols * rows);
+  for (let i = 0; i < 1000; i++) {
+    particles[i] = new Particle()
+  }
 }
 
 function draw() {
-  translate(width / 2, height / 2)
-  Image(img, 0, 0)
+    // background(500)
+    let yoff = 0;
+    
+    for (let y = 0; y < rows; y++) {
+      let xoff = 0;
+      for (let x = 0; x < cols; x++) {
+        let index = (x + y + cols);
+        let r = random() * TWO_PI
+        // let r = noise(xoff, yoff, zoff) * TWO_PI * 4;
+        let v = p5.Vector.fromAngle(r);
+        v.setMag(1);
+        flowField[index] = v;
+        xoff += inc;
+
+        // stroke(0);
+        // push();
+        // translate(x * scl, y * scl,);
+        // rotate(v.heading())
+        // line(0, 0 , scl, 0)
+
+        // pop();
+
+      }
+      yoff += inc;
+      zoff += 0.00005;
+    }
+    for (let i = 0; i < particles.length; i ++) {
+      particles[i].follow(flowField)
+      particles[i].update();
+      particles[i].show();
+      particles[i].edges()
+    }
 }
